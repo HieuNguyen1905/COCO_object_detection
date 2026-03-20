933cc23c670ff84f72bbdafecc2842d423fe198a
Hieu Nguyen Phuc <hieunp@technica.ai>
2026-03-20 14:52:45 +0700

    refactor code

--- src/datasets/datamodule.py
+++ src/datasets/datamodule.py
@@ -1,64 +1,37 @@
-from __future__ import annotations
-from pathlib import Path
 import torch
-import yaml
 from torch.utils.data import DataLoader
-from src.datasets.dataset import COCODetectionDataset, collate_fn
-from src.datasets.transforms import get_train_transforms, get_val_transforms
-
 class DataModule:
 
-    def __init__(self, config_path: str | Path):
-        # Load config
-        with open(config_path, "r") as f:
-            config = yaml.safe_load(f)
-        data_config = config["data"]
-        loader_config = config["dataloader"]
-        image_config = config["image"]
-
-        image_size = image_config["image_size"]
-        mean = image_config["mean"]
-        std = image_config["std"]
-
-        # Build augmentation pipelines
-        train_transforms = get_train_transforms(image_size, mean, std)
-        val_transforms = get_val_transforms(image_size, mean, std)
-
-        # Build datasets
-        self.train_dataset = COCODetectionDataset(
-            img_dir=data_config["train_images"],
-            ann_file=data_config["train_json"],
-            transforms=train_transforms,
-        )
-        self.val_dataset = COCODetectionDataset(
-            img_dir=data_config["val_images"],
-            ann_file=data_config["val_json"],
-            transforms=val_transforms,
-        )
-
-        # Build dataloaders
-        num_workers = loader_config["num_workers"]
-        # Only pin memory when a CUDA device is actually available
-        pin_memory = loader_config["pin_memory"] and torch.cuda.is_available()
-
-        self.train_loader = DataLoader(
+    def __init__(self, train_dataset, val_dataset, batch_size, num_workers,
+                 pin_memory, collate_fn):
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory and torch.cuda.is_available()
+        self.collate_fn = collate_fn
+        
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
             self.train_dataset,
-            batch_size=loader_config["batch_size"],
-            shuffle=loader_config["shuffle_train"],
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            collate_fn=collate_fn,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            collate_fn=self.collate_fn,
             drop_last=True,
         )
-        self.val_loader = DataLoader(
+    
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
             self.val_dataset,
-            batch_size=loader_config["batch_size"],
-            shuffle=loader_config["shuffle_val"],
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            collate_fn=collate_fn,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            collate_fn=self.collate_fn,
             drop_last=False,
         )
-
+    @property
     def num_classes(self) -> int:
         return self.train_dataset.num_classes
